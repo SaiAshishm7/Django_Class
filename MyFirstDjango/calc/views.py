54ea6db15b4b6e9ffa0edc9f118dd5dd2b826db6
SaiAshishM9 <mrichigo321@gmail.com>
2024-07-25 21:44:21 +0530

    DJANGO

--- MyFirstDjango/calc/views.py
+++ MyFirstDjango/calc/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render (request,'home.html',{'name':'cyrus'})
 
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    return render(request,'results.html',{'result':val3})
 # Create your views here.
